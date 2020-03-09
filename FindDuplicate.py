317b6bb195a341d71fb3a05b5f6cb4d0ddd832a2
suhird <suhirdsingh92@gmail.com>
2020-03-08 22:20:19 -0400

    first commit with 1 file

---
+++ FindDuplicate.py
@@ -0,0 +1,16 @@
+'''
+LeetCode Problem:-  Find Duplicates in an array of numbers
+
+Given an array of integers, 1<=a[i]<=n(n=size of array), some elements appear twice
+and others appear once
+
+Find all elements that appear twice in this array
+
+Could you do it in without extra space and in O(n) runtime
+
+Example
+input = [4,3,2,7,8,2,3,1]
+
+output = [2,3]
+
+'''
